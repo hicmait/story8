104b6ef99a23559a69ae2c049be4b396f83276ec
Hicham <hic.mait@gmail.com>
2025-06-04 09:35:23 +0100

    bidl

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,5 @@ export default defineConfig({
       // the proper extensions will be added
       fileName: "main",
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-        },
-      },
-    },
   },
 });
