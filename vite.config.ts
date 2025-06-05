b77817b42e658d4480d7ed8203f2b5ab8f413ccd
Hicham <hic.mait@gmail.com>
2025-06-05 15:59:47 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
+// import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // import path from 'path';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "src/lib/main.js"),
+      formats: ["es"],
       name: "tamtam-new",
       // the proper extensions will be added
       fileName: "main",
